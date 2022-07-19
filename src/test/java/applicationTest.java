540204a38b49320d0fc3ee53bf2fe1b49148a79e
장원영 <32123302+OzRagwort@users.noreply.github.com>
2022-07-19 15:32:40 +0900

    찐막 (#13)

---
+++ src/test/java/applicationTest.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class applicationTest {
+
+    @Test
+    void main() {
+        assertThat(1).isEqualTo(2);
+    }
+}
